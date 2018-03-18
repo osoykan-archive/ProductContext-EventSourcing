8832d38c9649bc282575cc5276ae10001e724c02
osoykan <oguzhansoykan@gmail.com>
2018-03-19 01:21:34 +0300

    create product integration test added

--- test/ProductContext.Integration.Tests/Product_Integration_Tests.cs
+++ test/ProductContext.Integration.Tests/Product_Integration_Tests.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
 
+using AggregateSource;
+using AggregateSource.EventStore;
+using AggregateSource.EventStore.Resolvers;
+
 using Couchbase.Core;
+using Couchbase.Search;
 
 using EventStore.ClientAPI;
 
+using ProductContext.Domain.Contracts;
+using ProductContext.Domain.Products;
+using ProductContext.Domain.Projections;
 using ProductContext.Framework;
 
 using Xunit;
@@ -29,6 +37,50 @@ namespace ProductContext.Integration.Tests
             IBucket bucket = getBucket();
 
             Assert.NotNull(bucket);
+        }
+
+        [Fact]
+        public async Task product_creation_integraiton_test()
+        {
+            IEventStoreConnection esConnection = await Defaults.GetEsConnection("admin", "changeit", "tcp://admin:changeit@127.0.0.1:1113");
+
+            var bus = new InMemoryBus("bus");
+
+            var defaultSerializer = new DefaultEventDeserializer();
+            var concurrentUnitOfWork = new ConcurrentUnitOfWork();
+
+            var productRepository = new AsyncRepository<Product>(
+                Product.Factory,
+                concurrentUnitOfWork,
+                esConnection,
+                new EventReaderConfiguration(
+                    new SliceSize(500),
+                    defaultSerializer,
+                    new PassThroughStreamNameResolver(),
+                    new NoStreamUserCredentialsResolver()));
+
+            DateTime GetDatetime()
+            {
+                return DateTime.UtcNow;
+            }
+
+            var productCommandHandlers = new ProductCommandHandlers((type, id) => $"{type.Name}-{id}", productRepository, GetDatetime);
+            bus.Subscribe<Commands.V1.CreateProduct>(productCommandHandlers);
+            bus.Subscribe<Commands.V1.AddVariantToProduct>(productCommandHandlers);
+            bus.Subscribe<Commands.V1.AddContentToProduct>(productCommandHandlers);
+
+            Func<IBucket> getBucket = Defaults.GetCouchbaseBucket(nameof(ProductContext), "Administrator", "123456", "http://localhost:8091");
+
+            await ProjectionManagerBuilder.With
+                                          .Connection(esConnection)
+                                          .Deserializer(new DefaultEventDeserializer())
+                                          .CheckpointStore(new CouchbaseCheckpointStore(getBucket))
+                                          .Projections(
+                                              ProjectorDefiner.For<ProductProjection>()
+                                          ).Activate(getBucket);
+
+
+            await bus.PublishAsync(new Commands.V1.CreateProduct() { BrandId = 1, BusinessUnitId = 1, Code = "CODE123" });
         }
     }
 }
